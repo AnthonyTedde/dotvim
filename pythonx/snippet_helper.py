546875055aaf0cee71546c92d2fc15dae8bb7bc4
Anthony Tedde <elpapitonino@gmail.com>
2014-08-08 11:47:25 +0200

    describe helper function classeName

--- pythonx/snippet_helper.py
+++ pythonx/snippet_helper.py
@@ -1,4 +1,10 @@
 from string import capwords
+'''
+Modify the file name from:
+something_like_this
+to:
+SomethingLikeThis
+'''
 def className(fileName):
     str = ''.join(capwords(fileName,"_").split("_"))
     return str
